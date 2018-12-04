02d94ed1d1dfb2e509c0f1993f78deb1058e4c00
Lucy <lucyneatdesign@yahoo.ca>
2018-12-04 00:12:48 +0000

    Done

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,6 @@ var request = require('request');
 var fs = require('fs');
 var token = require ('./secrets.js');
 
-
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -13,7 +12,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization': 'token ' + token.GITHUB_TOKEN
     }
   };
-
 
   request(options, function(err, res, body) {
     var jsonN = JSON.parse(body);
@@ -33,10 +31,9 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   if (err){
     console.log("Errors:", err);
   }
-  //console.log("Result:", result);
   for (var arr of result){
-    console.log(arr.avatar_url);
     downloadImageByURL(arr.avatar_url, "./avatars/" + arr.login + ".jpg");
   }
+  console.log("Download complete.");
 });
 
